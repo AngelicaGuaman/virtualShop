fdfd6c5448529aa6c56c5e4e558a2a409b3c77e4
Angélica Guamán Albarracín <angie90_05@hotmail.com>
2019-04-22 17:48:45 +0200

    modified controllers

---
+++ PracticaNETRoP/Controllers/ShoppingCardController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using PracticaNETRoP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaNETRoP.Controllers
+{
+    public class ShoppingCardController : Controller
+    {
+        private VirtualShopEntities db = new VirtualShopEntities();
+
+        // GET: ShoppingCard
+        public ActionResult Index(ShoppingCard sc)
+        {
+            return View(sc);
+        }
+
+        // GET: ShoppingCard/Delete/5
+        public ActionResult Delete(int? id, ShoppingCard sc)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Products products = sc.Find(x => x.Id == id);
+            if (products != null)
+            {
+                sc.Remove(products);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CreateOrder(ShoppingCard sc)
+        {
+            string email = User.Identity.GetUserName();
+
+         
+            return RedirectToAction("OrderCreated");
+        }
+    }
+}
